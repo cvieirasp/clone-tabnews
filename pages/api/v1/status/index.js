2669a32fd2bbf3ec7013411ae2612e0eb0a05fab
Carlos Vieira <cvieirasp@gmail.com>
2024-09-07 23:37:38 +0000

    complete `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,30 @@
 import database from "infra/database.js";
 
-const status = (request, response) => {
+const status = async (request, response) => {
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersion = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnections = dbMaxConnectionsResult.rows[0].max_connections;
+
+  const dbName = process.env.POSTGRES_DB;
+  const dbOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const dbOpenedConnections = dbOpenedConnectionsResult.rows[0].count;
+
   response.status(200).json({
-    status: "OK",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: parseInt(dbMaxConnections),
+        opened_connections: dbOpenedConnections,
+      },
+    },
   });
 };
 
